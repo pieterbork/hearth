33c18187aab1cf72198fc9125d6dc4476f3e02fc
pieterbork <pieter.bork@colorado.edu>
2018-04-29 13:20:38 -0600

    Put things in functions, updated with better examples, added config

--- README.md
+++ README.md
@@ -7,21 +7,48 @@ hearth is a simple dotfiles manager
 
 Download some sweet dotfiles
 ```
-$ python hearth.py https://github.com/pieterbork/dotfiles.git
-Cloning into '/home/rick/.hearth/pieterbork_dotfiles'...
-remote: Counting objects: 3, done.
-remote: Compressing objects: 100% (2/2), done.
-remote: Total 3 (delta 0), reused 3 (delta 0), pack-reused 0
-Unpacking objects: 100% (3/3), done.
+$ python hearth.py https://github.com/Airblader/dotfiles-manjaro.git
+Cloning into '/home/rick/.hearth/Airblader_dotfiles-manjaro'...
+remote: Counting objects: 1855, done.
+remote: Total 1855 (delta 0), reused 0 (delta 0), pack-reused 1855
+Receiving objects: 100% (1855/1855), 23.84 MiB | 11.75 MiB/s, done.
+Resolving deltas: 100% (993/993), done.
 Checking connectivity... done.
-Backing up /home/rick/.vimrc to /home/rick/.hearth/local_backup/.vimrc
-Copying /home/rick/.hearth/pieterbork_dotfiles/.vimrc to /home/rick/.vimrc
+Copying /home/rick/.hearth/Airblader_dotfiles-manjaro/.Xresources to /home/rick/.Xresources
+Copying /home/rick/.hearth/Airblader_dotfiles-manjaro/.bash.d to /home/rick/.bash.d
+Could not copy /home/rick/.hearth/Airblader_dotfiles-manjaro/.bash.d
+Copying /home/rick/.hearth/Airblader_dotfiles-manjaro/.bash_profile to /home/rick/.bash_profile
+Copying /home/rick/.hearth/Airblader_dotfiles-manjaro/.bashrc to /home/rick/.bashrc
+Copying /home/rick/.hearth/Airblader_dotfiles-manjaro/.compton.conf to /home/rick/.compton.conf
+Copying /home/rick/.hearth/Airblader_dotfiles-manjaro/.config to /home/rick/.config
+Copying /home/rick/.hearth/Airblader_dotfiles-manjaro/.gitconfig to /home/rick/.gitconfig
+Copying /home/rick/.hearth/Airblader_dotfiles-manjaro/.gtkrc-2.0 to /home/rick/.gtkrc-2.0
+Copying /home/rick/.hearth/Airblader_dotfiles-manjaro/.i3 to /home/rick/.i3
+Copying /home/rick/.hearth/Airblader_dotfiles-manjaro/.npmrc to /home/rick/.npmrc
+Copying /home/rick/.hearth/Airblader_dotfiles-manjaro/.vim to /home/rick/.vim
+Backed up /home/rick/.vimrc to /home/rick/.hearth/local_backup/.vimrc
+Copying /home/rick/.hearth/Airblader_dotfiles-manjaro/.vimrc to /home/rick/.vimrc
+Copying /home/rick/.hearth/Airblader_dotfiles-manjaro/.xinitrc to /home/rick/.xinitrc
 ```
 
 And restore back to your old ones
 ```
 $ python hearth.py local
+Deleting /home/rick/.Xresources
+Deleting /home/rick/.bash.d
+Deleting /home/rick/.bash_profile
+Deleting /home/rick/.bashrc
+Deleting /home/rick/.compton.conf
+Deleting /home/rick/.config
+Deleting /home/rick/.gitconfig
+Deleting /home/rick/.gtkrc-2.0
+Deleting /home/rick/.i3
+Deleting /home/rick/.npmrc
+Deleting /home/rick/.vim
+Deleting /home/rick/.vimrc
+Deleting /home/rick/.xinitrc
 Copying /home/rick/.hearth/local_backup/.vimrc to /home/rick/.vimrc
+Deleting /home/rick/.hearth/local_backup/
 ```
 
 That's it! Feedback is appreciated!
--- hearth.py
+++ hearth.py
@@ -1,39 +1,49 @@
 from os.path import expanduser
+import configparser
+import argparse
 import subprocess
 import shutil
 import sys
 import os
 
-ignores = ['.git', '.gitignore']
-
-repo = sys.argv[1]
-home = expanduser("~") + "/"
-
-hearth_home = home + ".hearth/"
-if not os.path.isdir(hearth_home):
+def create_dir(dir_name):
     try:
-        os.makedirs(hearth_home)
+        os.makedirs(dir_name)
     except:
         print("Cannot create {}".format(hearth_home))
         sys.exit(1)
 
-backup_time = False
-local_backup = hearth_home + "local_backup/"
-if not os.path.isdir(local_backup):
-    try:
-        os.makedirs(local_backup)
-        backup_time = True
-    except:
-        print("Cannot create {}".format(local_backup))
-        sys.exit(1)
+def get_args():
+    parser = argparse.ArgumentParser(description='Bring your dotfiles to you.')
+    parser.add_argument('repo', help='Either a github link or simply "local"')
+    args = parser.parse_args()
+    return args
 
-if repo == "local":
-    proj_home = local_backup
-else:
-    git_parts = repo.split('/')
+def get_config(config_file):
+    config = configparser.ConfigParser()
+    try:
+        config.read(config_file)
+    except:
+        print("Cannot read config file: {}".format(config_file))
+        sys.exit(1)
+    return config
+
+def save_config(config_file, config):
+    with open(config_file, 'w') as cf:
+        config.write(cf)
+
+def get_dotfiles(proj_home, ignores):
+    files = os.listdir(proj_home)
+    dotfiles = [f for f in files if f not in ignores and f.startswith('.')]
+    return dotfiles
+
+def parse_github_link(link):
+    git_parts = link.split('/')
     git_user = git_parts[-2]
-    proj_name = git_parts[-1].strip(".git")
-    proj_home = hearth_home + git_user + "_" + proj_name + "/"
+    proj_name = git_user + "_" + git_parts[-1].strip(".git") + "/"
+    return proj_name
+
+def download_repo(repo, proj_home):
     reset = True
     if os.path.isdir(proj_home):
         reset = input('Directory exists, redownload? (y/n):  ')
@@ -41,21 +51,90 @@ else:
             shutil.rmtree(proj_home)
         else:
             reset = False
-
     if reset:
         git_cmd = "git clone {} {}".format(repo, proj_home)
         p = subprocess.Popen(git_cmd, shell=True)
         p.wait()
 
-files = os.listdir(proj_home)
-dotfiles = [f for f in files if f not in ignores and not os.path.isdir(f)]
-for f in dotfiles:
-    src = proj_home + f
-    dst = home + f
-    if backup_time:
-        backup_path = local_backup + f
-        print("Backing up {} to {}".format(dst, backup_path))
-        os.rename(dst, backup_path)
-    print("Copying {} to {}".format(src, dst))
-    shutil.copyfile(src, dst)
+def initialize_hearth(hearth_home, repo):
+    if not os.path.isdir(hearth_home):
+        create_dir(hearth_home)
 
+    if not repo:
+        print("No repo provided!")
+        sys.exit(1)
+
+    backup_time = False
+    local_backup = hearth_home + "local_backup/"
+    if not os.path.isdir(local_backup):
+        create_dir(local_backup)
+        backup_time = True
+    return backup_time
+
+def delete_all(files, path=None):
+    if isinstance(files, str):
+        files = [files]
+    for f in files:
+        loc = f
+        if path:
+            loc = path + f 
+        print("Deleting {}".format(loc))
+        if os.path.isdir(loc):
+            shutil.rmtree(loc)
+        else:
+            os.remove(loc)
+
+def main():
+    args = get_args()
+    config = get_config('hearth.cfg')
+
+    home = expanduser("~") + "/"
+    hearth_home = home + ".hearth/"
+    local_backup = hearth_home + "local_backup/"
+    backup_time = initialize_hearth(hearth_home, args.repo)
+
+    if args.repo == "local":
+        proj_home = local_backup
+    else:
+        proj_name = parse_github_link(args.repo)
+        proj_home = hearth_home + proj_name
+        download_repo(args.repo, proj_home)
+
+    ignores = config['DEFAULT']['ignores']
+
+    old_proj = config.get('current', 'repo', fallback=None)
+    if old_proj:
+        dotfiles = get_dotfiles(old_proj, ignores)
+        delete_all(dotfiles, home)
+
+    dotfiles = get_dotfiles(proj_home, ignores)
+    for f in dotfiles:
+        src = proj_home + f
+        dst = home + f
+        if backup_time:
+            backup_path = local_backup + f
+            try:
+                shutil.move(dst, backup_path)
+                print("Backed up {} to {}".format(dst, backup_path))
+            except:
+                pass
+        print("Copying {} to {}".format(src, dst))
+        if os.path.isdir(src):
+            try:
+                shutil.copytree(src, dst)
+            except:
+                print("Could not copy {}".format(src))
+        else:
+            shutil.copyfile(src, dst)
+
+    if proj_home == local_backup:
+        delete_all(local_backup)
+        if old_proj:
+            config.remove_option('current', 'repo')
+            save_config('hearth.cfg', config)
+    else:
+        config.set('current', 'repo', proj_home)
+        save_config('hearth.cfg', config)
+
+if __name__ == "__main__":
+    main()
